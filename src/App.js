1e8bd2048541816b4f7cb1c54bcbc6e816c6e405
Jordan <jordan.langlois117@gmail.com>
2023-04-22 18:00:53 -0500

    list_api_changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { CssBaseline } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 import Account from './components/Content/Account/Account'
-//import List from './components/List/List';
+import List from './components/List/List';
+import { getPlacesData } from './api';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 import LoginContent from './components/Content/LoginContent';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
@@ -13,18 +14,61 @@ import Login from './components/Content/Login/Login';
 
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+    const [bounds, setBounds] = useState(null);
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('1');
+    const [price, setPrice] = useState('1');
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => 
+        place.rating > rating && place.price_level > price);
+
+        setFilteredPlaces(filteredPlaces);
+    }, [rating, price]);
+    
+
+    useEffect(() => {
+        if (bounds) {
+          getPlacesData(type, bounds.swlat, bounds.swlng, bounds.nelat, bounds.nelng)
+            .then((data) => {
+              console.log(data);
+              setPlaces(data);
+              setFilteredPlaces([])
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }, [type, bounds]);
+
     return (
         <>
             <CssBaseline />
             <Header />
+            <Grid container spacing={3} style={{ width: '100%'}} alignItems='center' justifyContent='center'>
+                <Grid item xs={12} md={5}>
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setBounds={setBounds}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                        price={price}
+                        setPrice={setPrice}
+                    />
+                </Grid>
+            </Grid>
             <BrowserRouter>
                <Routes>
                     <Route path="/" element={<Navigate replace to="/home" />} />
-
                     <Route path ="/home" element={<Content />} />
                     <Route path ="/account" element ={<Account />} />
                     <Route path ="/login" element ={<LoginContent />} />
-                    
                 </Routes>
             </BrowserRouter>
             <Footer />
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
-const PlaceDetails = () => {
+import useStyles from './styles';
+
+const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
+    
     return (
-        <h1>PlaceDetails</h1>
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350}}
+                image={place.photo ? place.photo.images.large.url : 'https://static0.thethingsimages.com/wordpress/wp-content/uploads/2022/02/9EB441A0-09D1-4872-BA59-D80510AD83BB.jpeg?q=50&fit=contain&w=1140&h=&dpr=1.5'}
+                place={place.name}>
+            </CardMedia>
+            <CardContent>
+                <Typography gutterBottom variant="h5"> 
+                    {place.name}
+                </Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterbottom variant="subtitle1">{place.price_level}</Typography>
+                    <Typography variant="subtitle1">Rating</Typography>
+                    <Typography gutterbottom variant="subtitle1">{place.rating}</Typography>
+                </Box>
+                {place?.cuisine?.map(( { name } ) =>
+                    <Chip key={name} size="small" label={name} className={classes.chip}></Chip>
+                )}
+            </CardContent>
+        </Card>
     );
 }
 
