adc55f3b0bab97152ab0af04ac8547568a3e004b
natannk06 <105123824+natannk06@users.noreply.github.com>
2023-04-24 15:16:02 -0500

    Add files via upload

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import Login from './Login/Login';
-import CreateAccount from './CreateAccount/CreateAccount';
+import React, { useState, useEffect } from 'react';
+import List from './List/List';
+import PlaceDetails from './PlaceDetails/PlaceDetails';
 import './Content.css'
+import { CssBaseline, Grid } from '@material-ui/core';
+import { getPlacesData } from '../../api';
 
-function Content() {
+
+function Content(){
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+  const [bounds, setBounds] = useState('eauclaire');
+
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('1');
+  const [price, setPrice] = useState('1');
+
+  useEffect(() => {
+      const filteredPlaces = places.filter((place) => 
+      place.rating > rating && place.price_level > price);
+
+      setFilteredPlaces(filteredPlaces);
+  }, [rating, price]);
+  
+
+  useEffect(() => {
+      if (bounds) {
+        getPlacesData(type, bounds.swlat, bounds.swlng, bounds.nelat, bounds.nelng)
+          .then((data) => {
+            console.log(data);
+            setPlaces(data);
+            setFilteredPlaces([])
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }, [type, bounds]);
+    
     return (
         <div class = 'body'>
-         
+          <div class = 'child'>
+         <Grid container spacing={3} style={{ width: '100%'}} alignItems='center' justifyContent='center'>
+                <Grid item xs={12} md={5}>
+                  
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setBounds={setBounds}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                        price={price}
+                        setPrice={setPrice}
+                    />
+                   
+                </Grid>
+            </Grid>
+
+            </div>
         </div>
     );
 }
