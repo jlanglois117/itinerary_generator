3c584c4b3851f41eabaa75313e5fa1f4344aa5a1
Jordan <jordan.langlois117@gmail.com>
2023-04-24 19:34:38 -0500

    fixed api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+const URL = 'https://travel-advisor.p.rapidapi.com/';
 
 
 export const getPlacesData = async (type, swlat, swlng, nelat, nelng) => {
     try {
-        const { data: { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+        const { data: { data }} = await axios.get(`${URL}${type}/list-in-boundary`, {
             params: {
                 bl_latitude: swlat,
                 tr_latitude: nelat,
